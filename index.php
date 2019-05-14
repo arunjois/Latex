2ef4fb65d15338f04f6476dbd6fe0beebaadff7a
Arun Jois <arun.joiss@gmail.com>
2019-05-14 19:44:30 +0530

    Something working on server side

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
-$tmp = $_POST["text"];
-echo $tmp;
+$section_title = (htmlspecialchars($_POST["section-title-1"]));
+$section_text = (htmlspecialchars($_POST["section-text-1"]));
+$myfile = fopen("test.tex", "w") ;
+$document_template = "\\documentclass{article}
+                      \\begin{document}";
+$title = "\\section{".$section_title."}";
+$subsection_title = "\\subsection{".$section_text."}";
+$text = "This is something";
+$end ="\\end{document}";
+fwrite($myfile,$document_template.$title.$subsection_title.$text.$end);
+fclose($myfile);
+
+
 
  ?>
