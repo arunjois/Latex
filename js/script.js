a45845087728721daf8578c15a3a70fb45dc5caa
Arun Jois <arun.joiss@gmail.com>
2019-05-14 16:40:29 +0530

    DOM changes working

--- js/script.js
+++ js/script.js
@@ -1,11 +1,30 @@
 function addSection() {
-  var div = document.createElement("label");
+  var sec = document.createElement("div");
+  sec.innerHTML="<label>Section</label>";
+  var ref = document.getElementById("ref");
+  ref.appendChild(sec);
+
+}
+function createTextArea() {
+  var div = document.createElement("textarea");
   div.setAttribute("name","section");
   div.innerHTML="Section";
-  var par = document.getElementById("section");
-  var inserted = document.getElementById("section");
-  inserted = par.appendChild(div,div);
-  console.log("Shoem");
+  console.log("Create TextArea");
+  return div;
+}
+function createLabel() {
+  var div = document.createElement("label");
+  div.setAttribute("name","section");
+  div.innerHTML="Section";2
+  console.log("Create Label");
+  return div;
+}
+function createInput() {
+  var div = document.createElement("input");
+  div.setAttribute("type","text");
+  div.setAttribute("name","text");
+  console.log("Create Input")
+  return div;
 }
 function addChapter() {
   console.log("KJ");
