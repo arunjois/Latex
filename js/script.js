8349c8635cb6a2e06af1d484f8ebb22d8cda28e2
Ashik gowda <ashikssgowda644@gmail.com>
2019-05-16 22:06:36 +0530

    [PATCH]Improved UI

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 function addSection() {
   var sec = document.createElement("div");
   sec.setAttribute("class","section");
-  var str = "<label>Section</label><br />"+"<input type='text' name='section-title[]' placeholder='side heading'/>"+
-  "<br /><textarea name='text[]' placeholder='text'></textarea>";
+  var str = "<label>Section</label><br />"+"<input type='text' class='form-control form-control-lg' name='section-title[]' placeholder='side heading'/>"+
+  "<br /><textarea name='text[]' placeholder='text' class='form-control form-control-lg' rows='4'></textarea>";
   sec.innerHTML=str;
   var ref = document.getElementById("ref");
   ref.appendChild(sec);
@@ -10,7 +10,7 @@ function addSection() {
 function createTextArea() {
   var sec = document.createElement("div");
   sec.setAttribute("class","text");
-  var str = "<br /><textarea name='text[]' placeholder='Text'></textarea>";
+  var str = "<br /><textarea name='text[]' class='form-control form-control-lg' rows='4' placeholder='Text'></textarea>";
   sec.innerHTML=str;
   var ref = document.getElementById("ref");
   ref.appendChild(sec); 
@@ -21,9 +21,9 @@ function addChapter() {
   var sec = document.createElement("div");
   sec.setAttribute("class","chapter");
   var str = "<label>Chapter</label><br />"+
-            "<input type='text' name='chapter[]' placeholder='Chapter Name' /> <br />"+
-            "<input type='text' name='section-title[]' placeholder='side heading'/>"+
-            "<br /><textarea name='text[]' placeholder='text'></textarea>";
+            "<input type='text' name='chapter[]' class='form-control form-control-lg' placeholder='Chapter Name' /> <br />"+
+            "<input type='text' name='section-title[]' class='form-control form-control-lg' placeholder='side heading'/>"+
+            "<br /><textarea name='text[]' class='form-control form-control-lg' rows='4'  placeholder='text'></textarea>";
   sec.innerHTML=str;
   var ref = document.getElementById("ref");
   ref.appendChild(sec);
